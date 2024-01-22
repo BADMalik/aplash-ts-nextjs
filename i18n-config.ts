224a00ba84bb4b23f81a9675f7294cc23991a24c
Bilal <bilal.malik@camp1.tkxel.com>
2024-01-22 16:04:40 +0500

    update package-lock

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,14 @@
+
 export const i18n = {
   defaultLocale: "en",
   locales: ["en", "ch", "ko", "jp"],
+} as const;
+
+export const nextI18NextConfig = {
+  i18n,
+  // Path to the translation files
+  // i.e., ./public/locales/en.json, ./public/locales/ch.json, etc.
+  localePath: './public/dictionaries',
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
