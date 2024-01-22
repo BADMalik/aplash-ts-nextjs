fd8fb93cfc4881d04338e3da011505e3beeb317f
Bilal <bilal.malik@camp1.tkxel.com>
2024-01-22 15:08:27 +0500

    Completed google sheets API

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -4,10 +4,10 @@ import type { Locale } from "./i18n-config";
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries = {
-  en: () => import("./dictionaries/en.json").then((module) => module.default),
-  jp: () => import("./dictionaries/jp.json").then((module) => module.default),
-  ch: () => import("./dictionaries/ch.json").then((module) => module.default),
-  ko: () => import("./dictionaries/ko.json").then((module) => module.default),
+  en: () => import("./public/dictionaries/en.json").then((module) => module.default),
+  jp: () => import("./public/dictionaries/jp.json").then((module) => module.default),
+  ch: () => import("./public/dictionaries/ch.json").then((module) => module.default),
+  ko: () => import("./public/dictionaries/ko.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) =>
