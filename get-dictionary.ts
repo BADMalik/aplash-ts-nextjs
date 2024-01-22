5de6556979bf758157a405d5bf2ca74c9edfcc7d
Bilal <bilal.malik@camp1.tkxel.com>
2024-01-22 17:01:28 +0500

    updated lamba function root path

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import type { Locale } from "./i18n-config";
-
+import { i18n } from "./i18n-config";
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries = {
@@ -10,5 +10,6 @@ const dictionaries = {
   ko: () => import("./public/dictionaries/ko.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale]?.() ?? dictionaries.en();
+// export const getDictionary = async (locale: Locale) =>
+//   dictionaries[locale]?.() ?? dictionaries.en();
+export const getDictionary = async (locale: Locale) => dictionaries[i18n.locales.includes(locale) ? locale : i18n.defaultLocale]()
--- i18n-config.ts
+++ i18n-config.ts
@@ -8,7 +8,7 @@ export const nextI18NextConfig = {
   i18n,
   // Path to the translation files
   // i.e., ./public/locales/en.json, ./public/locales/ch.json, etc.
-  localePath: './public/dictionaries',
+  localePath: `${process.env.LAMBDA_TASK_ROOT}/public/dictionaries`,
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
